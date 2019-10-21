eb696de00de184f7d5b2938ebab30b0fd23f1f92
yl131 <53383107+yl131@users.noreply.github.com>
2019-10-21 16:37:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -27,17 +27,9 @@ dill._dill._reverse_typemap['ClassType'] = type
 
 model = dill.load(open('lib/models/wine_estimator.dill','rb'))
 
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-@app.route('/label', methods=['GET','POST'])
+@app.route('/', methods=['GET','POST'])
 def label():
     return render_template('label.html')
-
-@app.route('/upload', methods=['GET','POST'])
-def upload():
-    return render_template('upload.html')
 
 @app.route('/prediction', methods=['POST'])
 def prediction():
